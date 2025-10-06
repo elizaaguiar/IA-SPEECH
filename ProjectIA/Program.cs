4c7bbeaab5e6c0330020df9d40b12431c02bf607
Eliza Aguiar <elizaaguiar2015@hotmail.com>
2025-10-06 16:00:49 -0300

    Add initial project files, README, and assets for Azure AI Speech classes

---
+++ ProjectIA/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vá em README.md para ver as anotações sobre os serviços de IA da Azure.");
+Console.WriteLine("Na pasta assets você encontrará prints do processo de aprendizagem.");
